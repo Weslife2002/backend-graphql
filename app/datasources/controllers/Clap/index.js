abbb0fc09794f8ede197c91fe59dbca765d7e31c
Weslife2002 <truongtranduytan@gmail.com>
2022-12-26 17:40:27 +0700

    Clap commands

---
+++ app/datasources/controllers/Clap/index.js
@@ -0,0 +1,13 @@
+const clapPost = require('./clapPost');
+const clapComment = require('./clapComment');
+const clapCount = require('./clapCount');
+const unClapComment = require('./unClapComment');
+const unClapPost = require('./unClapPost');
+
+module.exports = {
+  clapComment,
+  clapCount,
+  clapPost,
+  unClapComment,
+  unClapPost,
+};
