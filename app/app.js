b5de72046f4336abf9327a46eb8af31264c7296e
Weslife2002 <truongtranduytan@gmail.com>
2022-12-26 09:56:41 +0700

    create Apolor Server and express-app

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const createContext = require('./utils/createContext');
 const datasources = require('./datasources');
 const resolvers = require('./resolvers');
 const typeDefs = require('./schemas');
@@ -10,42 +11,11 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   dataSources: datasources,
-  context: ({ req, res }) => ({ req, res }),
+  context: createContext,
+  formatError: error => (error),
 });
 
 module.exports = {
   server,
   app,
 };
-
-// async function startServer() {
-//   const app = express();
-//   app.use(session({
-//     secret: 'thisismysecrctekeyfhrgfgrfrty84fwir767',
-//     store: new RedisStore({ client: redisClient }),
-//     saveUninitialized: true,
-//     cookie: {
-//       maxAge: config.sessionTimeOut,
-//       secure: false,
-//       httpOnly: true,
-//     },
-//     resave: true,
-//   }));
-
-//   const server = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-//     dataSources: datasources,
-//     context: ({ req, res }) => ({ req, res }),
-//   });
-
-//   await server.start();
-
-//   server.applyMiddleware({ app });
-
-//   app.listen(4000, () => {
-//     console.log(`🚀. Server ready at http://localhost:4000${server.graphqlPath}`);
-//   });
-// }
-
-// module.exports = startServer;
