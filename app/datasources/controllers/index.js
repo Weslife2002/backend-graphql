ec12548706b4026f903843fbaabad33431cae6ec
Weslife2002 <truongtranduytan@gmail.com>
2022-12-26 17:44:11 +0700

    Controller commands

--- app/datasources/controllers/index.js
+++ app/datasources/controllers/index.js
@@ -1,7 +1,13 @@
-const User = require('./User');
+const Clap = require('./Clap');
+const Comment = require('./Comment');
+const Follow = require('./Follow');
 const Post = require('./Post');
+const User = require('./User');
 
 module.exports = {
-  User,
+  Clap,
+  Comment,
+  Follow,
   Post,
+  User,
 };
