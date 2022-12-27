3609634a0b3d61bc613be73c761c6a8d6bbfd911
Weslife2002 <truongtranduytan@gmail.com>
2022-12-27 11:11:14 +0700

    MongoDB model

--- app/datasources/models/Clap.js
+++ app/datasources/models/Clap.js
@@ -4,8 +4,8 @@ const { ObjectId } = mongoose.Types;
 
 const Clap = new mongoose.Schema({
   user: { type: ObjectId, required: true },
-  post: { type: ObjectId, required: true },
-  postOwner: { type: ObjectId, required: true },
+  post: { type: ObjectId },
+  postOwner: { type: ObjectId },
   comment: { type: ObjectId },
 
   count: { type: Number, default: 1 },
