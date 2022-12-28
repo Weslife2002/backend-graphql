377698f2d0add9c72c1679863c4018fef16074d8
Weslife2002 <truongtranduytan@gmail.com>
2022-12-28 15:45:29 +0700

    update redis storage

--- app/datasources/utils/redis/clearUserCache.js
+++ app/datasources/utils/redis/clearUserCache.js
@@ -4,7 +4,7 @@ module.exports = async id => {
   let cursor = 0;
   let keys = [];
   do {
-    [cursor, keys] = redisClient.scan(cursor, `${id}:*`);
+    [cursor, keys] = redisClient.scan(cursor, 'MATCH', `${id}:*`, 'COUNT', '100');
     redisClient.del(keys);
   } while (cursor !== 0);
 };
