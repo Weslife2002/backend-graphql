64e7680c95dc09339b1ac57ce60e21776ee9c6e2
Weslife2002 <truongtranduytan@gmail.com>
2023-01-02 16:10:12 +0700

    Handle uncaught exception while running the app

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ const startServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
   app.listen(config.server.port, () => {
-    console.log(`🚀. Server ready at http://localhost:${config.server.port}${server.graphqlPath}`);
+    logger.info(`🚀. Server ready at http://localhost:${config.server.port}${server.graphqlPath}`);
   });
 
   process.on('uncaughtException', exception => {
